06c5b8841fa562e8a93b82f4db19d1ee64b79da8
표명선 <ms147@ktis.co.kr>
2022-12-16 16:52:52 +0900

    vue cli form test

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit="submitForm">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
       <input id="username" type="text" v-model="username">
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -21,9 +23,21 @@ export default {
     };
   },
   methods: {
-    submitForm(event){
-      event.preventDefault();   // submit은 기본적으로 새로고침 기능이 있음. 그걸 막아주는 event 함수
+    submitForm(){
+      // event.preventDefault();   // submit은 기본적으로 새로고침 기능이 있음. 그걸 막아주는 event 함수
       console.log(this.username + ", " + this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username : this.username,
+        password : this.password,
+      }
+      axios.post(url, data)
+          .then(function (response){
+            console.log(response)
+          })
+          .catch(function (error){
+            console.log(error);
+          });
     }
   },
 }
